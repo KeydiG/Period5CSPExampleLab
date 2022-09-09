f17847d4f0d825fa9d5d46b0e9f680dfd3a7c5a8
Keydi <keydi.garcia@vtsd.com>
2022-09-09 12:28:10 -0400

    Data types

---
+++ DataTypes.java
@@ -0,0 +1,16 @@
+// Single Line Comment Syntax
+/*
+     MultiLine comment syntax
+
+     Nothing in here will be executed(ran)
+
+ */
+public class DataTypes {
+
+    //Variable syntax: dataType varName = value;
+    int FavInt = 5;
+    double snicker = 7.5;
+    boolean likeDogs = true;
+
+
+}
